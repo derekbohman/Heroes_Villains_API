d392cd5dac8718ede60913442779d659000432e0
Derek Bohman <derekbohman@Dereks-MacBook-Pro.local>
2022-04-28 14:58:10 -0700

    Created Supers url file + Modified Views Files For Supers and SuperTypes.

--- supers/views.py
+++ supers/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from urllib import response
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import SuperSerializer
+from .models import Super
 
-# Create your views here.
+@api_view(['GET', 'POST'])
+def supers_list(request):
+    if request.method == 'GET':
+        super_type_name = request.query_params.get('super_type')
+        print(super_type_name)
+        queryset = Super.objects.all()
+
+        if super_type_name:
+            queryset = queryset.filter(super_type__name=super_type_name)
+
+        serializer = SuperSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = SuperSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def super_type_detail(request, pk):
+    super = get_object_or_404(Super, pk=pk)
+    if request.method == 'GET':   
+        serializer = SuperSerializer(super)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+         serializer = SuperSerializer(super, data=request.data)
+         serializer.is_valid(raise_exception=True)
+         serializer.save()
+         return Response(serializer.data)
+
+    elif request.method == 'DELETE':
+        super.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
